b7dce71b9339a360c8e7ea9b52a8303fedb0e8b6
sooji <yh0gogo@gmail.com>
2024-04-18 15:42:29 +0900

    add: api endpoint and payloads

---
+++ src/types/shares.ts
@@ -0,0 +1,25 @@
+export type Icon = {
+  type: 'text' | 'image';
+  data: {
+    url: string;
+  };
+};
+
+type FaviconItem = {
+  size: number | null;
+  url: string | null;
+};
+export type Favicon = {
+  common: FaviconItem[] | null;
+  android: FaviconItem[] | null;
+  ios: FaviconItem[] | null;
+};
+
+export type GetCommunityShareInformation = {
+  id: string;
+  name: string;
+  url: string | null;
+  icon: Icon | null;
+  favicon: Favicon | null;
+  locale: string | null;
+};

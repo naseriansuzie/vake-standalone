d3d963acd1a1ed2c9e1f85b945080471f2dcac0f
sooji <yh0gogo@gmail.com>
2024-04-18 15:41:43 +0900

    add: public envs

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,17 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
+// TODO: 서버 타입에 따라서 env 값 분기
+// const serverType = process.env.NEXT_PUBLIC_SERVER_TYPE;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compiler: {
     styledComponents: true,
+  },
+  env: {
+    NEXT_PUBLIC_SERVER_TYPE: process.env.NEXT_PUBLIC_SERVER_TYPE,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
 };
 

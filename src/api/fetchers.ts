ff3185daabddd681864315bfd6ec1bb511692e31
sooji <yh0gogo@gmail.com>
2024-04-18 15:42:06 +0900

    add: http method utils

---
+++ src/api/fetchers.ts
@@ -0,0 +1,97 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import _isNil from 'lodash-es/isNil';
+
+export interface CustomApiError extends Error {
+  reason?: string;
+  status?: number;
+}
+
+export interface FetchOptions {
+  body?: { [key: string]: any };
+  cookie?: string | null;
+  endpoint: string;
+  headers?: { [headerName: string]: string } | null;
+  method?: 'GET' | 'POST' | 'PUT' | 'DELETE';
+  params?: Record<string, string | boolean | number | undefined | null>;
+}
+
+const changeResponseToJson = async <T = Record<string, string>>(response: Response) => {
+  return response.json().then((json) => ({
+    response,
+    json: json as unknown as T,
+  }));
+};
+
+const cleanParams = (
+  params: { [key: string]: string | number | boolean | undefined | null } = {},
+) => {
+  const copiedParams = Object.assign({}, params) as { [key: string]: string | number | boolean };
+  Object.keys(copiedParams).forEach((key) => {
+    if (_isNil(copiedParams[key])) {
+      delete copiedParams[key];
+    }
+  });
+
+  return copiedParams;
+};
+
+const createCustomApiError = (json: any): CustomApiError => {
+  const error = new Error() as CustomApiError;
+  error.reason =
+    json.error.errors && json.error.errors.length > 0 ? json.error.errors[0].reason : '';
+  error.status = json.error.code;
+
+  return error;
+};
+
+const fetchJson = <T>({
+  body,
+  endpoint,
+  headers: extraHeaders,
+  method = 'GET',
+}: FetchOptions): Promise<T> => {
+  const headers: { [headerName: string]: string } = {
+    accept: 'application/json',
+  };
+
+  if (extraHeaders) {
+    Object.assign(headers, extraHeaders);
+  }
+
+  return fetch(`${process.env.NEXT_PUBLIC_APP_URL}${endpoint}`, {
+    method,
+    headers,
+    body: body ? JSON.stringify(body) : undefined,
+  })
+    .then(changeResponseToJson)
+    .then(({ response, json }) => {
+      const parsedBody = json as unknown as T;
+
+      if (response.ok) {
+        return { ...parsedBody };
+      }
+
+      throw createCustomApiError(json);
+    });
+};
+
+export const getJson = <T>({ endpoint, params, headers }: FetchOptions): Promise<T> => {
+  let qs = '';
+  const cleanedParams = cleanParams(params);
+
+  if (Object.keys(cleanedParams).length > 0) {
+    qs = `?${new URLSearchParams(cleanedParams as { [key: string]: any })}`;
+  }
+
+  return fetchJson<T>({
+    endpoint: `${endpoint}${qs}`,
+    headers,
+  });
+};
+
+const fetchers = {
+  get: getJson,
+  // TODO: post, delete 추가
+};
+
+export default fetchers;

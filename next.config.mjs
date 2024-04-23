b0889e06f487bdfcd043e1b0471df3343d853b7e
sooji <yh0gogo@gmail.com>
2024-04-23 17:49:38 +0900

    fix: update provider client ids as envs

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,16 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
-// TODO: 서버 타입에 따라서 env 값 분기
+// TODO: 서버 타입에 따라 배포 셋팅
 // const serverType = process.env.NEXT_PUBLIC_SERVER_TYPE;
+
+const kakaoKeys =
+  process.env.NODE_ENV === 'production'
+    ? { JS_KEY: process.env.KAKAO_JS_KEY, REST_API: process.env.KAKAO_REST_API }
+    : {
+        JS_KEY_TEST: process.env.KAKAO_JS_KEY_TEST,
+        REST_API_TEST: process.env.KAKAO_REST_API_TEST,
+      };
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -13,6 +21,12 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_SERVER_TYPE: process.env.NEXT_PUBLIC_SERVER_TYPE,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    PROVIDER: {
+      FACEBOOK: {
+        CLIENT_ID: process.env.FACEBOOK_CLIENT_ID,
+      },
+      KAKAO: kakaoKeys,
+    },
   },
 };
 

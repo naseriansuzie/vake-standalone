d02e77c8be2ca89f8bfb7ec3db68646ffbb64527
sooji <yh0gogo@gmail.com>
2024-04-28 22:17:38 +0900

    fix: add more font weight to load

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,7 +1,7 @@
 import { Noto_Sans_KR } from 'next/font/google';
 
 export const noto_sans_kr = Noto_Sans_KR({
-  weight: ['400', '600', '700'],
+  weight: ['300', '400', '600', '700'],
   style: ['normal'],
   subsets: ['latin'],
   display: 'swap',

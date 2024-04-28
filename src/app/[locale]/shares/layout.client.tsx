f4d4c63dbb4e177bffea6d494d3d7fc5e7de5d16
sooji <yh0gogo@gmail.com>
2024-04-28 22:53:41 +0900

    fix: hold vertical central when changing viewport

--- src/app/[locale]/shares/layout.client.tsx
+++ src/app/[locale]/shares/layout.client.tsx
@@ -1,8 +1,29 @@
 'use client';
 
+import { useEffect } from 'react';
 import styled from 'styled-components';
 
 const ClientSharesLayout = ({ children }: React.PropsWithChildren) => {
+  const setVhVariable = () => {
+    if (typeof window !== 'undefined') {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setVhVariable();
+      window.addEventListener('resize', setVhVariable);
+    }
+
+    return () => {
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('resize', setVhVariable);
+      }
+    };
+  }, []);
+
   return (
     <StyledSharesPage>
       <StyledContent>{children}</StyledContent>
@@ -16,12 +37,16 @@ const StyledSharesPage = styled.main`
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
   background: #f7f7f9;
+  overflow: hidden;
   margin: 0 auto;
 `;
 
 const StyledContent = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 95px;
   max-width: 500px;
 `;
 

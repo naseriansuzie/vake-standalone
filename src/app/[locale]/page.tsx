0f65e0cc904cc9f455133a04ff9d7e942fa7806a
nunonam <nunonam@gmail.com>
2024-05-21 15:12:17 +0900

    comingsoon

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,57 @@
+import React, { CSSProperties } from 'react';
+
 export default function Page() {
-  return <div>home</div>;
+  return (
+    <div style={containerStyle}>
+      <div style={overlayStyle}>
+        <span style={comingSoonStyle}>Coming Soon</span>
+        <div style={additionalTextContainerStyle}>
+          <span style={additionalTextStyle}>2024년 6월</span>
+          <span style={additionalTextStyle}>초대기능이 오픈됩니다</span>
+        </div>
+      </div>
+      <div>
+        home
+      </div>
+    </div>
+  );
 }
+
+const containerStyle: CSSProperties = {
+  position: 'relative',
+  height: '100vh',
+  width: '100vw',
+  overflow: 'hidden',
+};
+
+const overlayStyle: CSSProperties = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  height: '100%',
+  width: '100%',
+  backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+const comingSoonStyle: CSSProperties = {
+  color: 'white',
+  fontSize: '3em',
+  fontWeight: 'bold',
+  marginBottom: '10px',
+};
+
+const additionalTextContainerStyle: CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+};
+
+const additionalTextStyle: CSSProperties = {
+  color: 'white',
+  fontSize: '1.5em',
+};
+

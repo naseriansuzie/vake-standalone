74621d1864a99b1e4d1b25b7febcc043b6ea9ed1
sooji <yh0gogo@gmail.com>
2024-06-22 18:40:49 +0900

    remove: unused image and default page styling

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,19 +1,3 @@
-import Image from 'next/image';
-import ComingSoon from '@/assets/comingsoon.png';
-
-const styles = {
-  container: {
-    position: 'relative',
-    width: '100vw',
-    height: '100vh',
-    overflow: 'hidden',
-  } as React.CSSProperties,
-};
-
 export default function Page() {
-  return (
-    <div style={styles.container}>
-      <Image src={ComingSoon} alt="Coming Soon" layout="fill" objectFit="cover" quality={100} />
-    </div>
-  );
+  return <div />;
 }
